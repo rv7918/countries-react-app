ed42e09e79e025b6b06d57deae11c92db058953c
Rav Sohal <ravinder.sohal@bp.com>
2024-10-02 16:48:07 +0100

    update gridViewModel with loading and error state

--- src/components/grid/GridViewModel.tsx
+++ src/components/grid/GridViewModel.tsx
@@ -5,10 +5,21 @@ import { Country } from "./Grid.interface";
 
 const GridViewModel: React.FC = () => {
   const [gridData, setGridData] = useState<Country[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>(null);
 
   const getData = async () => {
-    const tableData = await getCountries();
-    setGridData(tableData);
+    try {
+      const tableData = await getCountries();
+      setGridData(tableData);
+      if (!tableData) {
+        throw new Error("Network response was not ok.");
+      }
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -19,7 +30,13 @@ const GridViewModel: React.FC = () => {
 
   return (
     <>
-      <GridViewList gridData={gridData} />
+      {loading ? (
+        <div className="mt-5">Loading...</div>
+      ) : error ? (
+        <>Error: {error}</>
+      ) : (
+        <GridViewList gridData={gridData} />
+      )}
     </>
   );
 };
