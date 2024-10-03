50090a1eb7f938536f1e4114d84cba05514eff11
Rav Sohal <ravinder.sohal@bp.com>
2024-10-04 00:43:36 +0100

    update test files

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -2,24 +2,31 @@ import { screen, render, act, cleanup } from "@testing-library/react";
 import { createRoot } from "react-dom/client";
 import "@testing-library/jest-dom";
 import Header from "./Header";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Header tests", () => {
   afterEach(() => {
     cleanup();
   });
 
+  const renderHeader = (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
+
   it("Should test the component and not crash", () => {
     const div = document.createElement("div");
     act(() => {
       const root = createRoot(div);
-      root.render(<Header />);
+      root.render(renderHeader);
       root.unmount();
     });
     expect(div.innerHTML).toBe("");
   });
 
   it("Should render the correct nav title", () => {
-    render(<Header />);
+    render(renderHeader);
     const navTitle = screen.getByText("React Countries");
     expect(navTitle).toBeInTheDocument();
   });
